f44d3c1ec97dcafd9aa9faf4941d0ce3ae3a4c62
Mathijs Lagerberg <sayhi@mlagerberg.com>
2022-07-13 13:33:49 +0200

    Improved evaluation printing

--- src/engine/fitness.c
+++ src/engine/fitness.c
@@ -48,9 +48,20 @@ extern inline int min(int a, int b);
 
 extern inline int get_pawn_count(int cache_pawn_count[2][8], int file, int color);
 
-void Fitness_debug(char *message, int value, int total) {
-	printf("%s%s:\t%s%d \t= %s%d%s\n", color_black, message, color_white, value, cyan, total, resetcolor);
+#ifdef PRINT_EVAL
+char *Fitness_square(int i, int j) {
+	char *str = malloc(3 * sizeof(char));
+	str[0] = i + 'a';
+	str[1] = '8' - j;
+	str[2] = '\0';
+	return str;
+} 
+void Fitness_debug(int i, int j, Piece *piece, char *message, int value, int total) {
+	char *tile = Fitness_square(i, j);
+	char *piece_color = piece->color == WHITE ? cyan : red;
+	printf("%s%s %s%s:\t%s%d \t= %s%d%s\n", piece_color, tile, color_black, message, color_white, value, cyan, total, resetcolor);
 }
+#endif
 
 int Fitness_calculate(Board *board) {
 	// Cache of the number of pawns for each player in each file
@@ -95,10 +106,7 @@ int Fitness_calculate(Board *board) {
 			}
 		}
 	}
-	#if PRINT_EVAL
-	Fitness_debug("head count white", head_count[1], 0);
-	Fitness_debug("head count black", head_count[0], 0);
-	#endif
+
 	assert (head_count[0] <= 16 && head_count[1] <= 16);
 	// Now, start analysis
 	int result = 0;
@@ -114,32 +122,32 @@ int Fitness_calculate(Board *board) {
 			if (piece->shape == PAWN) {
 				result += piece->color * MATERIAL_VALUE[PAWN];
 				#ifdef PRINT_EVAL
-				Fitness_debug("pawn\t\t", piece->color * MATERIAL_VALUE[PAWN], result);
+				Fitness_debug(i, j, piece, "pawn\t\t", piece->color * MATERIAL_VALUE[PAWN], result);
 				#endif
 				// Check for isolated (= badly defended) pawns
 				if (get_pawn_count(cache_pawn_count, i-1, piece->color) == 0 && get_pawn_count(cache_pawn_count, i+1, piece->color) == 0) {
 					result += piece->color * ISO_PENALTY[i];
 					#ifdef PRINT_EVAL
-					Fitness_debug("  bad defense", piece->color * ISO_PENALTY[i], result);
+					Fitness_debug(i, j, piece, "  bad defense", piece->color * ISO_PENALTY[i], result);
 					#endif
 				}
 				// Check for doubled pawns (= obstruction and bad defense)
 				if (get_pawn_count(cache_pawn_count, i, piece->color) > 1) {
 					result += piece->color * DOUBLE_PAWN_PENALTY;
 					#ifdef PRINT_EVAL
-					Fitness_debug("  doubled\t", piece->color * DOUBLE_PAWN_PENALTY, result);
+					Fitness_debug(i, j, piece, "  doubled\t", piece->color * DOUBLE_PAWN_PENALTY, result);
 					#endif
 				}
 				// Check for e and d pawns being blocked by opponent
 				if ((i==3 || i==4) && j==(piece->color == BLACK ? 4 : 3)) {
 					result += piece->color * E_AND_D_PENALTY;
 					#ifdef PRINT_EVAL
-					Fitness_debug("  e&d\t", piece->color * E_AND_D_PENALTY, result);
+					Fitness_debug(i, j, piece, "  e&d\t", piece->color * E_AND_D_PENALTY, result);
 					#endif
 					if (Board_is_color(board, i, j - piece->color, -piece->color)) {
 						result += piece->color * E_AND_D_BLOCKEDPENALTY;
 						#ifdef PRINT_EVAL
-						Fitness_debug("  e&d blocked", piece->color * E_AND_D_BLOCKEDPENALTY, result);
+						Fitness_debug(i, j, piece, "  e&d blocked", piece->color * E_AND_D_BLOCKEDPENALTY, result);
 						#endif
 					}
 				}
@@ -148,19 +156,19 @@ int Fitness_calculate(Board *board) {
 					if (max(abs(i - kings_pos[0][0]), abs(j - kings_pos[0][1])) <= 2) {
 						result += piece->color * PAWN_NEAR_KING_BONUS;
 						#ifdef PRINT_EVAL
-						Fitness_debug("  near king\t", piece->color * PAWN_NEAR_KING_BONUS, result);
+						Fitness_debug(i, j, piece, "  near king\t", piece->color * PAWN_NEAR_KING_BONUS, result);
 						#endif
 					}
 				} else if (max(abs(i - kings_pos[1][0]), abs(j - kings_pos[1][1])) <= 2) {
 					result += piece->color * PAWN_NEAR_KING_BONUS;
 					#ifdef PRINT_EVAL
-					Fitness_debug("  near king\t", piece->color * PAWN_NEAR_KING_BONUS, result);
+					Fitness_debug(i, j, piece, "  near king\t", piece->color * PAWN_NEAR_KING_BONUS, result);
 					#endif
 				}
 			} else if (piece->shape == KNIGHT) {
 				result += piece->color * MATERIAL_VALUE[KNIGHT];
 				#ifdef PRINT_EVAL
-				Fitness_debug("knight\t\t", piece->color * MATERIAL_VALUE[KNIGHT], result);
+				Fitness_debug(i, j, piece, "knight\t", piece->color * MATERIAL_VALUE[KNIGHT], result);
 				#endif
 				// Reward short distance to center
 				int distance = 0;
@@ -180,19 +188,19 @@ int Fitness_calculate(Board *board) {
 				}
 				result += piece->color * KNIGHT_CENTER_BONUS[distance];
 				#ifdef PRINT_EVAL
-				Fitness_debug("  near center\t", piece->color * KNIGHT_CENTER_BONUS[distance], result);
+				Fitness_debug(i, j, piece, "  near center", piece->color * KNIGHT_CENTER_BONUS[distance], result);
 				#endif
 				// Fine distance to either king
 				distance = abs(kings_pos[0][0] - i) + abs(kings_pos[0][1] - j)
 						 + abs(kings_pos[1][0] - i) + abs(kings_pos[1][1] - j);
 				result += piece->color * KNIGHT_KING_DIST_PER_TILE * distance;
 				#ifdef PRINT_EVAL
-				Fitness_debug("  distance to kings", piece->color * KNIGHT_KING_DIST_PER_TILE * distance, result);
+				Fitness_debug(i, j, piece, "  distance to kings", piece->color * KNIGHT_KING_DIST_PER_TILE * distance, result);
 				#endif
 			} else if (piece->shape == BISHOP) {
 				result += piece->color * MATERIAL_VALUE[BISHOP];
 				#ifdef PRINT_EVAL
-				Fitness_debug("bishop\t\t", piece->color * MATERIAL_VALUE[BISHOP], result);
+				Fitness_debug(i, j, piece, "bishop\t", piece->color * MATERIAL_VALUE[BISHOP], result);
 				#endif
 				// Reward Bishop when mobility is high
 				int mobility = v_get_rough_move_count_for_piece(board, i, j);
@@ -210,12 +218,12 @@ int Fitness_calculate(Board *board) {
 				}
 				result += piece->color * bonus;
 				#ifdef PRINT_EVAL
-				Fitness_debug("  mobility\t", piece->color * bonus, result);
+				Fitness_debug(i, j, piece, "  mobility\t", piece->color * bonus, result);
 				#endif
 			} else if (piece->shape == ROOK) {
 				result += piece->color * MATERIAL_VALUE[ROOK];
 				#ifdef PRINT_EVAL
-				Fitness_debug("rook\t\t", piece->color * MATERIAL_VALUE[ROOK], result);
+				Fitness_debug(i, j, piece, "rook\t\t", piece->color * MATERIAL_VALUE[ROOK], result);
 				#endif
 				// Reward Rook when mobility is high
 				int mobility = v_get_rough_move_count_for_piece(board, i, j);
@@ -233,25 +241,25 @@ int Fitness_calculate(Board *board) {
 				}
 				result += piece->color * bonus;
 				#ifdef PRINT_EVAL
-				Fitness_debug("  mobility\t", piece->color * bonus, result);
+				Fitness_debug(i, j, piece, "  mobility\t", piece->color * bonus, result);
 				#endif
 				// reward rook when no pawns are on the same file
 				if (get_pawn_count(cache_pawn_count, i,piece->color) == 0) {
 					result += piece->color * ROOK_NO_FRIENDLY_PAWNS_BONUS;
 					#ifdef PRINT_EVAL
-					Fitness_debug("  no friendly pawns", piece->color * ROOK_NO_FRIENDLY_PAWNS_BONUS, result);
+					Fitness_debug(i, j, piece, "  no friendly pawns", piece->color * ROOK_NO_FRIENDLY_PAWNS_BONUS, result);
 					#endif
 				}
 				if (get_pawn_count(cache_pawn_count, i,-piece->color) == 0) {
 					result += piece->color*ROOK_NO_ENEMY_PAWNS_BONUS;
 					#ifdef PRINT_EVAL
-					Fitness_debug("  no enemy pawns", piece->color * ROOK_NO_ENEMY_PAWNS_BONUS, result);
+					Fitness_debug(i, j, piece, "  no enemy pawns", piece->color * ROOK_NO_ENEMY_PAWNS_BONUS, result);
 					#endif
 				}
 			} else if (piece->shape == QUEEN) {
 				result += piece->color * MATERIAL_VALUE[QUEEN];
 					#ifdef PRINT_EVAL
-					Fitness_debug("queen\t\t", piece->color * MATERIAL_VALUE[QUEEN], result);
+					Fitness_debug(i, j, piece, "queen\t", piece->color * MATERIAL_VALUE[QUEEN], result);
 					#endif
 				// Fine for distance to own King
 				if (piece->color == BLACK) {
@@ -259,14 +267,14 @@ int Fitness_calculate(Board *board) {
 							* QUEEN_KING_DIST_PER_TILE
 							* (abs(kings_pos[0][0] - i) + abs(kings_pos[0][1] - j));
 					#ifdef PRINT_EVAL
-					Fitness_debug("  distance to king", piece->color * QUEEN_KING_DIST_PER_TILE * (abs(kings_pos[0][0] - i) + abs(kings_pos[0][1] - j)), result);
+					Fitness_debug(i, j, piece, "  distance to king", piece->color * QUEEN_KING_DIST_PER_TILE * (abs(kings_pos[0][0] - i) + abs(kings_pos[0][1] - j)), result);
 					#endif
 				} else {
 					result += piece->color
 							* QUEEN_KING_DIST_PER_TILE
 							* (abs(kings_pos[1][0] - i) + abs(kings_pos[1][1] - j));
 					#ifdef PRINT_EVAL
-					Fitness_debug("  distance to king", piece->color * QUEEN_KING_DIST_PER_TILE * (abs(kings_pos[0][0] - i) + abs(kings_pos[0][1] - j)), result);
+					Fitness_debug(i, j, piece, "  distance to king", piece->color * QUEEN_KING_DIST_PER_TILE * (abs(kings_pos[0][0] - i) + abs(kings_pos[0][1] - j)), result);
 					#endif
 				}
 			} else if (piece->shape == KING) {
@@ -292,9 +300,9 @@ int Fitness_calculate(Board *board) {
 				int bonus = (distance / 6) * KING_CENTER_BONUS[progress - 1];
 				int pawns = get_pawn_count(cache_pawn_count, i, -piece->color);
 				#ifdef PRINT_EVAL
-				Fitness_debug("king - game progress (.5x headcount)", progress, 0);
-				Fitness_debug("king - center distance", distance, 0);
-				Fitness_debug("king - resulting bonus", bonus, 0);
+				Fitness_debug(i, j, piece, "king - game progress (.5x headcount)", progress, 0);
+				Fitness_debug(i, j, piece, "king - center distance", distance, 0);
+				Fitness_debug(i, j, piece, "king - resulting bonus", bonus, 0);
 				#endif
 				if (i > 0) {
 					pawns = pawns + get_pawn_count(cache_pawn_count, i-1, -piece->color);
@@ -305,12 +313,12 @@ int Fitness_calculate(Board *board) {
 				if (pawns > 1) {
 					bonus = min(bonus, 0);
 					#ifdef PRINT_EVAL
-					Fitness_debug("  no bonus because enemy pawns are near", bonus, 0);
+					Fitness_debug(i, j, piece, "  no bonus because enemy pawns are near", bonus, 0);
 					#endif
 				}
 				result += piece->color * bonus;
 				#ifdef PRINT_EVAL
-				Fitness_debug("  result\t", piece->color * bonus, result);
+				Fitness_debug(i, j, piece, "  result\t", piece->color * bonus, result);
 				#endif
 			}
 		}
@@ -319,7 +327,7 @@ int Fitness_calculate(Board *board) {
 		int random = (rand() % RANDOM_FACTOR);
 		result += random;
 		#ifdef PRINT_EVAL
-		Fitness_debug("random\t\t", random, result);
+		Fitness_debug(i, j, piece, "random\t\t", random, result);
 		#endif
 	#endif
 	return result;
